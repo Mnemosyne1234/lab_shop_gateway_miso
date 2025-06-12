70d5b1af6c92290b26b0ace29491753488dc23af
ChoiMiSo <127930590+Mnemosyne1234@users.noreply.github.com>
2025-06-12 13:48:55 +0900

    commit

---
+++ inventory/src/main/java/labshopgatewaymiso/domain/DecreaseStockCommand.java
@@ -0,0 +1,11 @@
+package labshopgatewaymiso.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class DecreaseStockCommand {
+
+    private Integer qty;
+}
---
+++ monolith/src/main/java/labshopgatewaymiso/infra/OrderHateoasProcessor.java
@@ -0,0 +1,17 @@
+package labshopgatewaymiso.infra;
+
+import labshopgatewaymiso.domain.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderHateoasProcessor
+    implements RepresentationModelProcessor<EntityModel<Order>> {
+
+    @Override
+    public EntityModel<Order> process(EntityModel<Order> model) {
+        return model;
+    }
+}
